6f08bd9a85101e477a222913d142ff33baac7358
balusena <balusena12@gmail.com>
2024-01-11 21:39:12 +0000

    roboshop terraform files

---
+++ main.tf
@@ -0,0 +1,40 @@
+#Creating AWS Security Group
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allow_ssh_cidr
+  }
+
+  ingress {
+    from_port   = 5672
+    to_port     = 5672
+    protocol    = "tcp"
+    cidr_blocks = var.sg_subnet_cidr
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  instance_type          = var.instance_type
+  ami                    = data.aws_ami
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
+  tags                   = merge({ Name = "${var.component}-${var.env}" }, var.tags)
+  subnet_id              = var.subnet_id
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "component" {}
+variable "env" {}
+variable "instance_type" {}
+variable "sg_subnet_cidr" {}
+variable "tags" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "allow_ssh_cidr" {}
