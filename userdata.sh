dedc7c589d73736648b91fc6d8489017e2643619
balusena <balusena12@gmail.com>
2024-01-13 01:29:01 +0000

    roboshop terraform files

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/balusena/roboshop-ansible main.yml -e role_name=rabbitmq -e
- env=${env} &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/balusena/roboshop-ansible main.yml -e role_name=rabbitmq -e env=${env} &>>/opt/ansible.log
